2eb146ec0d9ef8f0388c1d65e52760299317442a
Wilquu22 <74911958+Wilquu22@users.noreply.github.com>
2020-11-23 14:25:03 +0100

    Add files via upload

---
+++ zad1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a, b, c;
+
+	cout << "podaj pierwsza liczbe";
+	cin >> a;
+
+	cout << "podaj druga liczbę";
+	cin >> b;
+
+	cout << "podaj trzecia liczbe";
+	cin >> c;
+
+	if (a > b && b > c)
+
+		cout << "srodkowa liczba to:";
+	cout << b;
+
+	if (c > b && b > a)
+		cin >> b;
+
+	if (b < c && a > c)
+		cin >> b;
+
+	
+
+	return 0;
+
+}
