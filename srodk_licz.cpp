50d9bc3120306e566980e1bae4f81ea47da8b8b7
Wilquu22 <74911958+Wilquu22@users.noreply.github.com>
2020-11-23 14:49:21 +0100

    Add files via upload

---
+++ srodk_licz.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a, b, c;
+
+	cout << "podaj pierwsza liczbe";
+	cin >> a;
+
+	cout << "podaj druga liczbę";
+	cin >> b;
+
+	cout << "podaj trzecia liczbe";
+	cin >> c;
+
+	if (a > b && b > c)
+
+		cout << "srodkowa liczba to:";
+	cout << b;
+
+	return 0;
+
+}
